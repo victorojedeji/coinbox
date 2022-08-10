35d0f644d7251f16e408b2b9470953569788886f
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-08-10 20:59:33 +0100

    setting up menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,27 @@ const Navbar = () => {
       <div className='logo-container'>
             <Avatar src={icon} size="large" />
             <Typography.Title level={2} className='logo'>
-              <Link to="/">coinbox</Link>
+              <Link to="/">Coinbox</Link>
             </Typography.Title>
 
-            {/* <Button className="menu-control-container"></Button> */}
+            <Menu theme="dark">
+              <Menu.Item icon={<HomeOutlined />}>
+                <Link to="/">Home</Link>
+              </Menu.Item>
 
+              <Menu.Item icon={<FundOutlined />}>
+                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+              </Menu.Item>
 
-      </div>
+              <Menu.Item icon={<MoneyCollectOutlined />}>
+                <Link to="/exchanges">Exchanges</Link>
+              </Menu.Item>
+
+              <Menu.Item icon={<BulbOutlined />}>
+                <Link to="/news">News</Link>
+              </Menu.Item>
+            </Menu>
+       </div>
     </div>
   )
 }
