e7438fd6917461b38b442e8394890faf7d88f92a
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-08-10 17:52:46 +0100

    setting up Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,23 @@ import { Button, Menu, Typography, Avatar} from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
-
+import icon from '../images/coinbox.png'
 
 const Navbar = () => {
   return (
     <div className='nav-container'>
       <div className='logo-container'>
-            <Avatar />
+            <Avatar src={icon} size="large" />
             <Typography.Title level={2} className='logo'>
-                <Link to="/">coinbox</Link>
+              <Link to="/">coinbox</Link>
             </Typography.Title>
+
+            {/* <Button className="menu-control-container"></Button> */}
+
+
       </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
