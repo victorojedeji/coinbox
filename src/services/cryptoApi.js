3d8e451cc4cbc3d525ed93c8b9a3326ea73e864d
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-08-11 19:01:13 +0100

    setting up redux toolkit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,19 @@
+
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       'tiers[0]': '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': '95c919cc2amsh132713150e90350p1df59ejsn6699ee2d5550',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
