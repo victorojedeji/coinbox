4fd90147494cebcc3b90a31b2634f5ba5ae77f7e
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-08-14 20:27:59 +0100

    fetch title data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -12,7 +12,7 @@ const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery();
   console.log(data)
 
-  // const globalStats = data?.data?.stats;
+  const globalStats = data?.data?.stats;
 
   if(isFetching) return 'Loading...'
 
@@ -22,15 +22,15 @@ const Homepage = () => {
         <Title level={2} className="heading">Global Crpyto Stats</Title>
 
         <Row>
-          <Col span={12}><Statistic title="Total Cryptocurrencies" value={5}/></Col>
+          <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
 
-          <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
+          <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.exchanges)}/></Col>
 
-          <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
+          <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
 
-          <Col span={12}><Statistic title="Total 24h Volume" value="5"/></Col>
+          <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
 
-          <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
+          <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
         </Row>
       </>
 
